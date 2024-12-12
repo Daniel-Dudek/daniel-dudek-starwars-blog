2fcea1cdf078cea9c61e5ea2ac7adf6cbecc6350
Dudi <69299652+Daniel-Dudek@users.noreply.github.com>
2024-12-12 08:19:22 +0000

    Todo list with fetch project done

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,33 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Container, Form, ListGroup } from "react-bootstrap";
 
-const initialList = [
-  {
-    task: "Make the bed",
-    id: crypto.randomUUID(),
-  },
-  {
-    task: "Wash my hands",
-    id: crypto.randomUUID(),
-  },
-];
 const TodoList = () => {
-  const [itemList, setItemList] = useState(initialList);
   const [inputValue, setInputValue] = useState("");
   const [current, setCurrent] = useState(null);
+  const [todos, setTodos] = useState([]);
+
+  // Function get todos list
+  const getTodoList = () => {
+    fetch("https://playground.4geeks.com/todo/users/Daniel", {
+      method: "GET", // POST, PUT, DELETE
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((response) => {
+        //console.log(response)
+        setTodos(response.todos);
+      })
+      .catch((error) => console.error("Error fetching todo list:", error));
+  };
+
+  // This useEffect function will be executed only once, when the component is finally loaded for the first time.
+  useEffect(() => {
+    getTodoList();
+  }, []);
 
   const handleAddTask = () => {
     if (inputValue.trim() === "") return;
-    const newTask = {
-      task: inputValue,
-      id: crypto.randomUUID(),
-    };
-    setItemList([...itemList, newTask]);
-    setInputValue("");
+    fetch("https://playground.4geeks.com/todo/todos/Daniel", {
+      method: "POST",
+      body: JSON.stringify({
+        label: inputValue,
+        is_done: false,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(() => {
+        getTodoList();
+        setInputValue("");
+      })
+      .catch((error) => console.error("Error adding task:", error));
   };
 
   const handleDeleteTask = (id) => {
-    setItemList(itemList.filter((item) => item.id !== id));
+    fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        getTodoList();
+      })
+      .catch((error) => console.error("Error deleting task:", error));
   };
 
   const handleKeyDown = (e) => {
@@ -43,7 +68,7 @@ const TodoList = () => {
         <Form.Control
           type="text"
           placeholder={
-            itemList.length === 0
+            todos.length === 0
               ? "There are no tasks, add new task"
               : "Add a new task"
           }
@@ -53,14 +78,14 @@ const TodoList = () => {
         />
       </div>
       <ListGroup className="my-4">
-        {itemList.map((item) => (
+        {todos.map((item) => (
           <ListGroup.Item
             key={item.id}
             className="d-flex justify-content-between align-items-center"
             onMouseEnter={() => setCurrent(item.id)}
             onMouseLeave={() => setCurrent(null)}
           >
-            {item.task}
+            {item.label}
             {current === item.id && (
               <Button
                 variant="danger"
